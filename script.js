a44e4b934d8d29aed6578841493b803103985174
kenjikubo03241 <kenjikubo03242@gmail.com>
2025-02-05 00:02:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const apiKey = 'cuh2nqpr01qva71sq3u0cuh2nqpr01qva71sq3ug'; // ここにFinnhubのAPIキーを入力
+
 async function fetchUSDJPY() {
-    const url = "https://query1.finance.yahoo.com/v8/finance/chart/USDJPY=X?interval=1m";
+    const url = `https://finnhub.io/api/v1/forex/rates?base=USD&symbols=JPY&token=${apiKey}`;
 
     try {
         const response = await fetch(url);
         const data = await response.json();
         
         // 最新の価格を取得
-        const price = data.chart.result[0].meta.regularMarketPrice;
+        const price = data.rates.JPY;
         
         // HTMLに表示
         document.getElementById("usd-jpy-rate").textContent = price.toFixed(2);
